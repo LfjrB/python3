56ada73189c51b18e500e23899edc12ed16beaea
LfjrB <jrlook.jb@gmail.com>
2018-07-15 18:06:28 -0300

    Ex014

---
+++ ex014.py
@@ -0,0 +1,5 @@
+c = float(input('Digite a temperatura em Celsius: '))
+#f = (c * 1.8) + 32 # formula 1
+f = ((9*c)/5)+32 # formula 2
+
+print('A temperatura de {}ºC corresponde a {}ºF!'.format(c, f))
